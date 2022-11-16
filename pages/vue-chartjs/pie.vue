83b13256c708bb4658d054e384f5006d1bce96d4
Tuan. Nguyen Anh - CMC Global DU1.6 <natuan20@cmcglobal.vn>
2022-11-16 17:27:34 +0700

    16.11

---
+++ pages/vue-chartjs/pie.vue
@@ -0,0 +1,50 @@
+
+<template>
+    <div class="container">
+        <Pie v-if="loaded" :chart-data="chartData" />
+    </div>
+</template>
+  
+<script>
+import { Pie } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale)
+
+export default {
+    name: 'BarChart',
+    components: { Pie },
+    data: () => ({
+        loaded: true,
+        chartData: {
+            label: 'Data One',
+            labels: ['React', 'VueJs', 'Nuxt', "tuan", "nga"],
+            datasets: [{
+                label: 'TuancanDongSang',
+                data: [30, 40, 25, 20, 17],
+                backgroundColor: '#f87979',
+            }],
+        },
+        chartOptions: {
+            responsive: true
+        }
+    }),
+    created() {
+    },
+    methods: {
+        tranfromdataID(arr) {
+            return arr.map(item => item.id)
+
+        },
+        tranfromdataName(arr) {
+            return arr.map(item => item.name)
+
+        }
+    }
+}
+</script>
+<style scoped>
+.container {
+    width: 1000px;
+}
+</style>
